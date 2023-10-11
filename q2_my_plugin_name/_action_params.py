b3b3c4de63ea0da2067e10f1596dc0a354c33963
Christos Matzoros <christosmatzoros@gmail.com>
2023-10-11 10:40:30 +0200

    Change input parameters

--- q2_my_plugin_name/_action_params.py
+++ q2_my_plugin_name/_action_params.py
@@ -9,13 +9,9 @@
 from qiime2.core.type import Bool, Choices, Float, Int, List, Range, Str
 
 apply_statistics_params = {
-    "cpus": Int % Range(1, None),
     "debug": Bool,
-    "seed": Int % Range(0, None),
 }
 # fmt: off
 apply_statistics_param_descriptions = {
-    "cpus": "Number of cpus to use.",
     "debug": " Enable debug logging.",
-    "seed": "Seed for all the random number generators.",
 }
